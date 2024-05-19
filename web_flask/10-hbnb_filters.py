71a44567cddf46903442b7c0228f1d23f0e15b7e
“Achraf <achrafassouam@gmail.com>
2024-05-19 15:05:10 +0100

    10-hbnb_filters.py -- 11th task

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
-"""
-Flask Application
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /hbnb_filters: HBnB HTML filters page.
 """
 from models import storage
-from flask import Flask, render_template
-from os import environ
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """
-    Remove the current SQLAlchemy Session after each request
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """
-    Displays the main HBNB filters HTML page
-    """
-    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
-    amenities = sorted(list(storage.all('Amenity').values()), key=lambda x: x.name)
-    cities = sorted(list(storage.all('City').values()), key=lambda x: x.name)
-
-    return render_template('10-hbnb_filters.html',
-                           states=states,
-                           amenities=amenities,
-                           cities=cities)
-
-
 if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, debug=True)
+    app.run(host="0.0.0.0")
